3c65a693183f62758c4378be29583f8679167503
Nalicyclica <siruri0xf3@gmail.com>
2021-07-29 14:24:59 +0900

    itemsコントローラー　new,createの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+  def index
+  end
+  def new
+    @item = Item.new()
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params_keys = [:title, :content, :category_id, :status_id, :delivery_pay_id, :prefecture_id, :expected_waiting_id, :price]
+    params.require(:item).permit(params_keys).merge(user_id: current_user.id)
+  end
 end
