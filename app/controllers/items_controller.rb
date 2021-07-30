184971366882e8af11b302c65e6898ff6bea519a
Nalicyclica <siruri0xf3@gmail.com>
2021-07-30 14:35:38 +0900

    Revert "読み込むパラメーターの修正"
    This reverts commit 1f23ffe57ed26b8f9ef700a2d173a34732a907ad.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
@@ -20,8 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params_keys = [:title, :content, :category_id, :status_id, :delivery_pay_id, :prefecture_id, :expected_waiting_id, :price,
-                   :image]
+    params_keys = [:title, :content, :category_id, :status_id, :delivery_pay_id, :prefecture_id, :expected_waiting_id, :price]
     params.require(:item).permit(params_keys).merge(user_id: current_user.id)
   end
 end
