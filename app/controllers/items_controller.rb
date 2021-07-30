508873f666de116b5268e29b388f9d27ffc5d5f3
Nalicyclica <siruri0xf3@gmail.com>
2021-07-30 14:36:43 +0900

    再：読み込むパラメーターの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params_keys = [:title, :content, :category_id, :status_id, :delivery_pay_id, :prefecture_id, :expected_waiting_id, :price]
+    params_keys = [:title, :content, :category_id, :status_id, :delivery_pay_id, :prefecture_id, :expected_waiting_id, :price,
+                   :image]
     params.require(:item).permit(params_keys).merge(user_id: current_user.id)
   end
 end
