f7e9022202fbcc4849e66549b4f447249b2d93dd
Nalicyclica <siruri0xf3@gmail.com>
2021-08-02 11:28:31 +0900

    レビュー箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update1]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :item_find, only: [:show, :edit, :update]
+  before_action :not_item_owner, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,17 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
-    redirect_to root_path unless @item.user_id == current_user.id
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,5 +40,13 @@ class ItemsController < ApplicationController
     params_keys = [:title, :content, :category_id, :status_id, :delivery_pay_id, :prefecture_id, :expected_waiting_id, :price,
                    :image]
     params.require(:item).permit(params_keys).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
+  def not_item_owner
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 end
