3c2d19c4110f904e43db92661fec53940f70d2f0
Nalicyclica <siruri0xf3@gmail.com>
2021-08-03 16:01:26 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,9 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    if @order_address.save
+    if @order_address.valid?
+      credit_pay(@order_address)
+      @order_address.save
       redirect_to root_path
     else
       @item = Item.find(params[:item_id])
@@ -18,6 +20,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params_key = [:post_number, :prefecture_id, :city, :address, :apartment, :tel]
-    params.require(:order_address).permit(params_key).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(params_key).merge(user_id: current_user.id, item_id: params[:item_id],
+                                                            token: params[:token])
+  end
+
+  def credit_pay(order_address)
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_address.ordered_item.price,
+      card: order_address.token,
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :address, :apartment, :tel, :order_id
+  attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :address, :apartment, :tel, :order_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -10,16 +10,18 @@ class OrderAddress
     validates :prefecture_id
     validates :city
     validates :address
-
     validates :tel, format: { with: /\A\d{10,11}\z/, allow_blank: true, message: 'must be 10 or 11 digits number' }
-    validates :order_id
+
+    validates :token
   end
 
   def save
-    return unless valid?
-
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, address: address, apartment: apartment,
                    tel: tel, order_id: order.id)
+  end
+
+  def ordered_item
+    Item.find(item_id)
   end
 end
