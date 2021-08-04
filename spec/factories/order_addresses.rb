37ea31fda32b021e30beb7b1e9629b6c68ea21c5
Nalicyclica <siruri0xf3@gmail.com>
2021-08-04 10:47:57 +0900

    テストコードの追加と修正
    MySqlとのconnectが切れるので、同期的に処理するように変更

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :order_address do
     post_number { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     prefecture_id { Faker::Number.within(range: 0..46) }
-    city {Faker::Address.city}
-    address {Faker::Address.street_address}
-    apartment {Faker::Address.secondary_address}
-    tel {Faker::Number.leading_zero_number(digits: 10)}
-    token {"tok_#{Faker::Internet.password(min_length: 20, max_length: 30, mix_case: true, special_characters: true)}"}
+    city { Faker::Address.city }
+    address { Faker::Address.street_address }
+    apartment { Faker::Address.secondary_address }
+    tel { Faker::Number.leading_zero_number(digits: 10) }
+    token { "tok_#{Faker::Internet.password(min_length: 20, max_length: 30, mix_case: true, special_characters: true)}" }
   end
 end
